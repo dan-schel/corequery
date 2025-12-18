c86c0f945bbd9f9b03957b0caea46d724d886c95
dan-schel <me@danschellekens.com>
2025-12-18 20:22:07 +1100

    add pwa assets

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,7 +8,38 @@ export default defineConfig({
     preact(),
     VitePWA({
       manifest: {
-        name: "CoreQuery Demo App",
+        name: "CoreQuery",
+        short_name: "CoreQuery",
+        description:
+          "A platform for building web apps serving transit information.",
+        theme_color: "#242933",
+        background_color: "#242933",
+        icons: [
+          {
+            src: "/favicon-circle-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/favicon-circle-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/favicon-maskable-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/favicon-maskable-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
       },
     }),
   ],
