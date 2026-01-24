2c4cf9f92f45575d5b00a910fb9cb8bf566499af
dan-schel <me@danschellekens.com>
2026-01-24 21:26:55 +1100

    export lintable types

--- server/lint-config.ts
+++ server/lint-config.ts
@@ -1,8 +1,8 @@
 import type { CorequeryConfig } from "./config/config.js";
 
-type LintableConfig = Omit<CorequeryConfig, "assets" | "port">;
+export type LintableConfig = Omit<CorequeryConfig, "assets" | "port">;
 
-type LintOptions = {
+export type LintOptions = {
   pass?: boolean;
   // ignoreStopsMissingPlatforms: boolean;
 };
@@ -16,3 +16,23 @@ export function lintConfig(_config: LintableConfig, options: LintOptions = {}) {
 
   throw new Error("Config is invalid - not implemented yet!");
 }
+
+/*
+
+TODO: To lint:
+
+- url names are as expected
+- stops have at least one platform (or none do)
+- no duplicate stop ids, line ids, platform ids within a stop, route ids within 
+  a line
+- asset paths exist on disk
+- all lines have codes (or none do)
+- no duplicate routes in the same line or across different lines
+- routes have at least two non-hidden stops
+- line diagrams have at least one entry
+- line diagrams cover every non-hidden stop on every route
+- order of stops on line diagram doesn't disagree with any routes
+- all lines are covered by the lines page tags, and none are covered twice
+- all stops have locations (or none do)
+
+*/
