37f27ecff796cfea0bd83e6277f03488b8af5fa5
dan-schel <me@danschellekens.com>
2026-02-01 16:24:58 +1100

    note

--- server/config/linting/checks/lines-page/index.ts
+++ server/config/linting/checks/lines-page/index.ts
@@ -5,6 +5,9 @@ import { noLinesMissingCheck } from "./no-lines-missing.js";
 export const linesPageCheck: LintingCheck = {
   scope: "linesPage",
   fn: (ctx) => {
+    // Don't need to check if any sections are provided. As long as there's
+    // lines, those lines will be required to appear in sections.
+
     ctx.run(noLinesMissingCheck);
     ctx.run(noLinesDuplicatedCheck);
   },
