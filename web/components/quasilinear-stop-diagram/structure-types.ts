b4130010878c28a066fe7f191e515d0bf1e8a5c1
dan-schel <me@danschellekens.com>
2026-04-30 21:44:10 +1000

    todos would you believe

--- web/components/quasilinear-stop-diagram/structure-types.ts
+++ web/components/quasilinear-stop-diagram/structure-types.ts
@@ -10,11 +10,14 @@ export type LinearStopDiagramStructure = {
   readonly type: "linear";
   readonly stops: readonly StopStructure[];
 
-  // TODO: Allow setting a threshold for the faded out section.
+  // TODO: Allow setting a threshold for the faded out section. Only linear
+  // needs it, as it's only for the service page.
 
   // TODO: Allow "Trains typically start from/continue to [other lines]" for
   // the Pakenham, Cranbourne, and Sunbury lines (and Sandringham, Werribee, and
-  // Williamstown lines in future).
+  // Williamstown lines in future). Only linear needs it for now, as it doesn't
+  // make sense for loop lines, and V/Line (the only user of branch lines)
+  // doesn't through run.
 };
 
 export type BranchStopDiagramStructure = {
