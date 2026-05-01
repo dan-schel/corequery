0436d9208a680eda7d9a2cddd3a24a8f37b7958b
dan-schel <me@danschellekens.com>
2026-05-01 19:28:36 +1000

    move ref inside

--- web/components/quasilinear-stop-diagram/QuasilinearStopDiagram.tsx
+++ web/components/quasilinear-stop-diagram/QuasilinearStopDiagram.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from "preact/hooks";
 import type { QuasilinearStopDiagramStructure } from "@/web/components/quasilinear-stop-diagram/structure-types";
 import { assertNever } from "@dan-schel/js-utils";
 import { BranchLayout } from "@/web/components/quasilinear-stop-diagram/branch/BranchLayout";
@@ -13,25 +12,6 @@ type QuasilinearStopDiagramProps = {
 };
 
 export function QuasilinearStopDiagram(props: QuasilinearStopDiagramProps) {
-  const contentParentRef = useRef<HTMLDivElement>(null);
-
-  // Bit a hack, but if we pass the ref directly to
-  // <Canvas>, it uses the initial value of `null` and
-  // doesn't notice when the ref updates to the actual div element. It does
-  // notice this change in state though.
-  //
-  // TODO: This hack doesn't work if the structure type changes without the
-  // component being unmounted/remounted. I'm currently using a key to achieve
-  // this, but it shouldn't be the code which USES this component that has to
-  // deal with it. Can I find a better solution?
-  const [contentParent, setContentParent] = useState<HTMLDivElement | null>(
-    null,
-  );
-
-  useEffect(() => {
-    setContentParent(contentParentRef.current);
-  }, []);
-
   if (props.structure.type === "linear") {
     return (
       <LinearLayout
@@ -39,8 +19,6 @@ export function QuasilinearStopDiagram(props: QuasilinearStopDiagramProps) {
         structure={props.structure}
         lightThemeColorHexCode={props.lightThemeColorHexCode}
         darkThemeColorHexCode={props.darkThemeColorHexCode}
-        contentParent={contentParent}
-        contentParentRef={contentParentRef}
       />
     );
   } else if (props.structure.type === "branch") {
@@ -50,8 +28,6 @@ export function QuasilinearStopDiagram(props: QuasilinearStopDiagramProps) {
         structure={props.structure}
         lightThemeColorHexCode={props.lightThemeColorHexCode}
         darkThemeColorHexCode={props.darkThemeColorHexCode}
-        contentParent={contentParent}
-        contentParentRef={contentParentRef}
       />
     );
   } else if (props.structure.type === "loop") {
@@ -61,8 +37,6 @@ export function QuasilinearStopDiagram(props: QuasilinearStopDiagramProps) {
         structure={props.structure}
         lightThemeColorHexCode={props.lightThemeColorHexCode}
         darkThemeColorHexCode={props.darkThemeColorHexCode}
-        contentParent={contentParent}
-        contentParentRef={contentParentRef}
       />
     );
   } else {
--- web/components/quasilinear-stop-diagram/linear/LinearLayout.tsx
+++ web/components/quasilinear-stop-diagram/linear/LinearLayout.tsx
@@ -1,4 +1,3 @@
-import type { RefObject } from "preact";
 import type { LinearStopDiagramStructure } from "@/web/components/quasilinear-stop-diagram/structure-types";
 import clsx from "clsx";
 import { Grid } from "@/web/components/core/Grid";
@@ -6,7 +5,13 @@ import {
   Canvas,
   type CreateControllerFunc,
 } from "@/web/components/canvas/Canvas";
-import { useCallback, useMemo } from "preact/hooks";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "preact/hooks";
 import { useSettings } from "@/web/hooks/use-settings";
 import type { QuasilinearStopDiagramCanvasData } from "@/web/components/quasilinear-stop-diagram/quasilinear-diagram-canvas-controller";
 import { LinearDiagramCanvasController } from "@/web/components/quasilinear-stop-diagram/linear/linear-diagram-canvas-controller";
@@ -16,24 +21,41 @@ type LinearLayoutProps = {
   structure: LinearStopDiagramStructure;
   lightThemeColorHexCode: string | null;
   darkThemeColorHexCode: string | null;
-  contentParent: HTMLDivElement | null;
-  contentParentRef: RefObject<HTMLDivElement>;
 };
 
 export function LinearLayout(props: LinearLayoutProps) {
   const { settings } = useSettings();
+
+  const contentParentRef = useRef<HTMLDivElement>(null);
+
+  // Bit a hack, but if we pass the ref directly to
+  // <Canvas>, it uses the initial value of `null` and
+  // doesn't notice when the ref updates to the actual div element. It does
+  // notice this change in state though.
+  //
+  // TODO: This hack doesn't work if the structure type changes without the
+  // component being unmounted/remounted. I'm currently using a key to achieve
+  // this, but it shouldn't be the code which USES this component that has to
+  // deal with it. Can I find a better solution?
+  const [contentParent, setContentParent] = useState<HTMLDivElement | null>(
+    null,
+  );
+
+  useEffect(() => {
+    setContentParent(contentParentRef.current);
+  }, []);
 
   const data = useMemo<QuasilinearStopDiagramCanvasData>(
     () => ({
       structure: props.structure,
       lightThemeColorHexCode: props.lightThemeColorHexCode,
       darkThemeColorHexCode: props.darkThemeColorHexCode,
-      contentParent: props.contentParent,
+      contentParent,
       colorTheme: settings.theme,
     }),
     [
+      contentParent,
       props.darkThemeColorHexCode,
-      props.contentParent,
       props.lightThemeColorHexCode,
       props.structure,
       settings.theme,
@@ -55,7 +77,7 @@ export function LinearLayout(props: LinearLayoutProps) {
         createController={createController}
         data={data}
       />
-      <div ref={props.contentParentRef} class="flex flex-col gap-6">
+      <div ref={contentParentRef} class="flex flex-col gap-6">
         {props.structure.stops.map((stop) => (
           <Grid>{stop.content}</Grid>
         ))}
