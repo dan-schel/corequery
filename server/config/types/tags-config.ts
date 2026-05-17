126dc343280b9e456d09991d63f890679d27219f
dan-schel <me@danschellekens.com>
2026-05-17 20:54:35 +1000

    Export diagram stop extractor helper

--- server/config/types/tags-config.ts
+++ server/config/types/tags-config.ts
@@ -1,6 +1,7 @@
 export type TagsConfig = {
   readonly stopTagSuccession: TagSuccessionConfig;
   readonly lineTagSuccession: TagSuccessionConfig;
+  readonly serviceTagSuccession: TagSuccessionConfig;
 };
 
 export type TagSuccessionConfig = Readonly<Record<number, readonly number[]>>;
